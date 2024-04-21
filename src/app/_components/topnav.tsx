518bc9b8b9fd9cd47dca56e71969a90344098352
suhanlim <soohan2009@naver.com>
2024-04-21 23:29:37 +0900

    FEAT: file uploading logic

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNev() {
+  const router = useRouter();
+
   return (
     <nav className="flex items-center justify-between w-full border-b p-4
       text-xl font-semibold">
       <div>Gallery</div>
 
-      <div>
+      <div className="flex flex-row">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }} />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ async function Images() {
   });
   return (
     <div className="flex flex-wrap gap-4">
-      {[...images, ...images, ...images].map((image, index) => (
-        <div key={image.id + "-" + index} className="flex flex-col w-48">
+      {images.map((image) => (
+        <div key={image.id} className="flex flex-col w-48">
           <img src={image.url} />
           <div>{image.name}</div>
         </div>
