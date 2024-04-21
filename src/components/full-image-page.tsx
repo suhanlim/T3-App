585b56910e4b8e3460095bcb05eacb6cd7a9f8d7
suhanlim <soohan2009@naver.com>
2024-04-22 01:11:33 +0900

    CLN: more clean up ui

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,6 +3,14 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
   return (
-    <img src={image.url} className="w-96" />
+    <div className="flex w-full h-full ">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} className="flex-shrink object-contain" />
+      </div>
+      <div className="w-48 flex flex-col flex-shrink-0 border-l">
+        <div className="text-xl font-bold">{image.name}
+        </div>
+      </div>
+    </div>
   );
 }
