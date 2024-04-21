067b9c81283cc2d54326521c62bbb31e821abb55
suhanlim <soohan2009@naver.com>
2024-04-21 21:26:01 +0900

    FEAT: scaffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,16 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNev() {
+  return (
+    <nav className="flex items-center justify-between w-full border-b p-4
+      text-xl font-semibold">
+      <div>Gallery</div>
+      <div>Sign In</div>
+    </nav>
+  )
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -20,7 +30,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+        <TopNev />
+        {children}
+      </body>
     </html>
   );
 }
