b19cdda025ad89bd27b736f15e388a9fdf31da44
suhanlim <soohan2009@naver.com>
2024-04-22 00:46:57 +0900

    FEAT: implementing parallel routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { db } from "~/server/db";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
 import Image from "next/image";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
@@ -11,11 +12,13 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="flex flex-col w-48">
-          <Image src={image.url}
-            width={192}
-            height={192}
-            style={{ objectFit: "contain" }}
-            alt={image.name} />
+          <Link href={`/img/${image.id}`}>
+            <Image src={image.url}
+              width={192}
+              height={192}
+              style={{ objectFit: "contain" }}
+              alt={image.name} />
+          </Link>
           <div>{image.name}</div>
         </div>
       ))}
