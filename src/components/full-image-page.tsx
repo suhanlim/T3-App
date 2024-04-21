a600de30722a997b169dff71822d4257dcc07c41
suhanlim <soohan2009@naver.com>
2024-04-22 01:24:00 +0900

    FEAT: Routing image page

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,14 +1,25 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex w-full h-full ">
       <div className="flex flex-shrink items-center justify-center">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
       <div className="w-48 flex flex-col flex-shrink-0 border-l">
-        <div className="text-xl font-bold">{image.name}
+        <div className="border-b text-lg text-center font-bold p-2">{image.name}
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Uploadeed By:</span>
+          <span>{uploaderInfo.fullName}</span>
+        </div>
+        <div className="flex flex-col p-2">
+          <span>Created On:</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
         </div>
       </div>
     </div>
